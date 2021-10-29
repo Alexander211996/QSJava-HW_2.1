84329f660bd8729649fd264ed1267ae985bb794b
Alexander Morningstar <rubolov.alexander@mail.ru>
2021-10-29 20:46:08 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 3_000;
+        int rubForOneMile = 20;
+        int milesForTicket = ticketPrice / rubForOneMile;
+        System.out.println(milesForTicket);
+    }
+}
